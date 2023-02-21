455adcbba27f48b5c6e9037ad2d994ff5bcdfd40
fawsiyyah <fawsiyyahlamidi@gmail.com>
2023-02-21 08:21:31 +0100

    new commit

--- index.js
+++ index.js
@@ -14,11 +14,24 @@ else if ( dept == 'art') {
 }
 
 //Question 5
-let arr = []
-function getSquares(){
-for (let i =2; i<2000; i *= 2){
-    arr.push(i)
-}
-console.log(arr)
-}
-getSquares()
+// let arr = []
+// let bingo = ``
+// function getSquares(number){
+// for (let i =2; i<20000; i *= 2){
+//     arr.push(i)
+// }
+// let forwards = arr.filter(j => (j - number) < number)
+// for (let k = 0; k < forwards.length; k++){
+//     if (Math.abs(forwards[k] - number) <= Math.abs(forwards[k + 1] - number)) {
+//         bingo = forwards[k]
+//     }else if ((Maths.abs(forwards[k] - number) >= Math.abs(forwards[k+ 1] - number)) && forwards[k + 1] > number ){
+//     bingo = forwards[k + 1]
+//     }
+// }console.log(`The number ${bingo} is the power of 2 nearest to ${number}`)
+// }
+// getSquares(30)
+
+let num = 50;
+for(let index =2; index < 20000; index = index**2) {
+    pwr = index
+} console.log("The number " + pwr + " is the power of 2 nearest to " + num);
